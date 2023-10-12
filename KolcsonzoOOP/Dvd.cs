efdf7f9306a7914a378930d2f556d9f08608fbc0
Petrik Diak <diak@petrik.hu>
2023-10-12 09:03:25 +0200

    Dvd class

--- KolcsonzoOOP/Dvd.cs
+++ KolcsonzoOOP/Dvd.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace KolcsonzoOOP
 {
-	internal class Dvd
+	internal class Dvd : IKolcsonozheto
 	{
+		private string cim;
+		private int hossz; // percben.
+
+		public string Cim { get => cim; set => cim = value; }
+		public int Hossz { get => hossz; set => hossz = value; }
+
+		public Dvd(string cim, int hossz)
+		{
+			this.cim = cim;
+			this.hossz = hossz;
+		}
+
+		public int MeddigKolcsonozheto()
+		{
+			return Convert.ToInt32(7);
+		}
+
+		public string MegjelenitendoNev()
+		{
+			return $"{this.cim} ({this.hossz}) perc.";
+		}
 	}
 }
