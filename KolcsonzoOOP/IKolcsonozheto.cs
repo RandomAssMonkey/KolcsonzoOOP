7aa6d09cd1624da44af501bcabdd94b8ee3c6afe
Petrik Diak <diak@petrik.hu>
2023-10-12 08:36:02 +0200

    Starting with interface

---
+++ KolcsonzoOOP/IKolcsonozheto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolcsonzoOOP
+{
+	public interface IKolcsonozheto
+	{
+		string MegjelenitendoNev();
+		int MeddigKolcsonozheto();
+	}
+}
