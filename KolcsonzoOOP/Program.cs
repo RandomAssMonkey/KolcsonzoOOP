79539b176c2b8494db78d9137f3bbbcb03352074
Petrik Diak <diak@petrik.hu>
2023-10-12 09:20:41 +0200

    all of them

--- KolcsonzoOOP/Program.cs
+++ KolcsonzoOOP/Program.cs
@@ -1,5 +1,21 @@
 ﻿
+using KolcsonzoOOP;
 
+List<IKolcsonozheto> katalogus = new List<IKolcsonozheto>();
+
+Konyv k1 = new Konyv("Golyók","Tóth Marcell", "420-6-9");
+Dvd d1 = new Dvd("Evil Bong", 90);
+Ujsag u1 = new Ujsag("Népszava", 2009,16);
+
+katalogus.Add(d1);
+katalogus.Add(k1);
+katalogus.Add(u1);
+
+
+foreach (var k in katalogus)
+{
+	Console.WriteLine(k.MegjelenitendoNev()+ " ~ " + k.MeddigKolcsonozheto() + "nap.");
+}
 
 
 
---
+++ KolcsonzoOOP/Ujsag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolcsonzoOOP
+{
+	internal class Ujsag : IKolcsonozheto
+	{
+		private string cim;
+		private int kiadasEv;
+		private int kiadasHet;
+
+		public string Cim { get => cim; set => cim = value; }
+		public int KiadasEv { get => kiadasEv; set => kiadasEv = value; }
+		public int KiadasHet { get => kiadasHet; set => kiadasHet = value; }
+
+		public Ujsag(string cim, int kiadasEv, int kiadasHet)
+		{
+			this.cim = cim;
+			this.kiadasEv = kiadasEv;
+			this.kiadasHet = kiadasHet;
+		}
+
+		public int MeddigKolcsonozheto()
+		{
+			return Convert.ToInt32(0);
+		}
+
+		public string MegjelenitendoNev()
+		{
+			return $"{this.cim} ~ {this.kiadasEv}/{this.kiadasHet}";
+		}
+	}
+}
